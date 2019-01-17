0976013419fe504cea83447a965226a677bf7ef5
Pavel Ermolaev <ermolaevp@gmail.com>
2019-01-17 19:49:26 +0300

    Updates

---
+++ precache-manifest.ec0f91e3acd1d33f6a4cf0a51f215c14.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd1e84a1ded645768c5b",
+    "url": "/my-contacts-client/static/js/main.fd1e84a1.chunk.js"
+  },
+  {
+    "revision": "9bee6b669b9f3bead146",
+    "url": "/my-contacts-client/static/js/1.9bee6b66.chunk.js"
+  },
+  {
+    "revision": "636537ab3e0f42153059",
+    "url": "/my-contacts-client/static/js/runtime~main.636537ab.js"
+  },
+  {
+    "revision": "c326f9fa149906adaf90235e94227dce",
+    "url": "/my-contacts-client/index.html"
+  }
+];
